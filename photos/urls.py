69ba12fd4034e5297cc105135cdb6efed3c92c20
Ryan Kirsch <agalloch88@gmail.com>
2026-03-12 14:04:40 -0400

    feat: JWT auth endpoints (register, login, refresh)

--- photos/urls.py
+++ photos/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = []
+from photos.views import RegisterView
+
+urlpatterns = [
+    # Auth
+    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
